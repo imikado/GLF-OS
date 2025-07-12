7813e70b6a6b67c4f0d7857205ca6002bc65ee00
Cammi <118620964+camini@users.noreply.github.com>
2025-07-12 11:53:38 +0200

    Update studio.nix

--- modules/default/environments/studio.nix
+++ modules/default/environments/studio.nix
@@ -7,60 +7,71 @@
 }:
 
 {
-  config = lib.mkIf(config.glf.environment.enable && (config.glf.environment.edition == "studio" || config.glf.environment.edition == "studio-pro")) {
-    systemd.tmpfiles.rules =
-      let
-        rocmEnv = pkgs.symlinkJoin {
-          name = "rocm-combined";
-           paths = with pkgs.rocmPackages; [
-            rocblas
-            hipblas
-            clr
-          ];
-         };
-      in [
-        "L+    /opt/rocm/hip  -    -    -      -    ${rocmEnv}"
-       ];
+    config = lib.mkIf(config.glf.environment.enable && (config.glf.environment.edition == "studio" || config.glf.environment.edition == "studio-pro")) {
+boot.extraModulePackages = [
+    (pkgs.linuxKernel.packages.linux_6_12.v4l2loopback.overrideAttrs
+      ({
+        version = "0.13.2-manual";
+        src = (pkgs.fetchFromGitHub {
+          owner = "umlaeute";
+          repo = "v4l2loopback";
+          rev = "v0.13.2";
+          hash = "sha256-rcwgOXnhRPTmNKUppupfe/2qNUBDUqVb3TeDbrP5pnU=";
+        });
+      })
+    )
+  ];
 
-    hardware.graphics = {
-     enable = true;
-      extraPackages = with pkgs-unstable; [
-        mesa.opencl
-      ];
-     };
-
-    environment.variables = {
-      ROC_ENABLE_PRE_VEGA = "1";
-      RUSTICL_ENABLE = "radeonsi";
+systemd.services.flatpak-repo = {
+      wantedBy = [ "multi-user.target" ];
+      requires = [ "network-online.target" ];
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+      path = [ pkgs.flatpak ];
+      script = ''
+        	flatpak install -y com.obsproject.Studio org.blender.Blender org.kde.kdenlive 
+      '';
     };
+systemd.tmpfiles.rules = 
+  let
+    rocmEnv = pkgs.symlinkJoin {
+      name = "rocm-combined";
+      paths = with pkgs.rocmPackages; [
+        rocblas
+        hipblas
+        clr
+      ];
+    };
+  in [
+    "L+    /opt/rocm/hip  -    -    -     -    ${rocmEnv}"
+  ];  
 
+        hardware.graphics = {
+            enable = true; 
+            extraPackages = with pkgs-unstable; [
+            mesa.opencl # Assure que l'implémentation OpenCL de Mesa (Rusticl) est installée
+            ];
+          };
+
+        environment.variables = {
+          ROC_ENABLE_PRE_VEGA = "1";
+          RUSTICL_ENABLE = "radeonsi"; 
+        };
+    
     environment.systemPackages =
       if config.glf.environment.edition == "studio-pro" then
-        with pkgs; [
-          blender-hip
-          obs-studio
-          obs-studio-plugins.obs-vkcapture
-          kdePackages.kdenlive
+        with pkgs-unstable; [
           davinci-resolve-studio
           gimp3-with-plugins
           audacity
           freetube
-        ]
+          ]
       else
-        with pkgs; [
-          blender-hip
-          obs-studio
-          obs-studio-plugins.obs-vkcapture
-          kdePackages.kdenlive
+        with pkgs-unstable; [
           davinci-resolve
           gimp3-with-plugins
           audacity
-          freetube
-        ];
-
-    programs.obs-studio = {
-      enable=true;
-      package = pkgs.obs-studio.override {cudaSupport = true;};
-    };
+          freetube  
+          ];
   };
 }
